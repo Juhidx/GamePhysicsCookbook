eb944ade3debce07d6e023008289e84e231ca4fd
Juho Salonoja <juhid@hotmail.com>
2024-12-22 13:41:49 +0200

    Continuing Matrix library

--- MathLibrary/MathLibrary/matrices.cpp
+++ MathLibrary/MathLibrary/matrices.cpp
@@ -72,24 +72,24 @@ void Transpose(const float* srcMat, float* dstMat, int srcRows, int srcCols)
     }
 }
 
-mat2 Transpose(const mat2& matrix)
+mat2 Transpose(const mat2& mat)
 {
     mat2 result;
-    Transpose(matrix.asArray, result.asArray, 2, 2);
+    Transpose(mat.asArray, result.asArray, 2, 2);
     return result;
 }
 
-mat3 Transpose(const mat3& matrix)
+mat3 Transpose(const mat3& mat)
 {
     mat3 result;
-    Transpose(matrix.asArray, result.asArray, 3, 3);
+    Transpose(mat.asArray, result.asArray, 3, 3);
     return result;
 }
 
-mat4 Transpose(const mat4& matrix)
+mat4 Transpose(const mat4& mat)
 {
     mat4 result;
-    Transpose(matrix.asArray, result.asArray, 4, 4);
+    Transpose(mat.asArray, result.asArray, 4, 4);
     return result;
 }
 
@@ -118,4 +118,496 @@ bool Multiply(float* out, const float* matA, int aRows, int aCols, const float* 
     }
 
     return true;
+}
+
+float Determinant(const mat2& mat)
+{
+    return mat._11 * mat._22 - mat._12 * mat._21;
+}
+
+float Determinant(const mat3& mat)
+{
+    float result = 0.0f;
+    int index;
+    mat3 cofactor = Cofactor(mat);
+
+    for (int i = 0; i < 3; ++i)
+    {
+        index = 3 * 0 + i;
+        result += mat.asArray[index] * cofactor[0][i];
+    }
+    
+    return result;
+}
+
+float Determinant(const mat4& mat)
+{
+    float result = 0.0f;
+    mat4 cofactor = Cofactor(mat);
+
+    for (int i = 0; i < 4; ++i)
+    {
+        result += mat.asArray[4 * 0 + i] * cofactor[0][i];
+    }
+
+    return result;
+}
+
+
+mat2 Cut(const mat3& mat, int row, int col)
+{
+    mat2 result;
+    int index = 0;
+    int target;
+    int source;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            if(i == row || j == col)
+            {
+                continue;
+            }
+
+            target = index++;
+            source = 3 * i + j;
+            result.asArray[target] = mat.asArray[source];
+        }
+    }
+
+    return result;
+}
+
+mat3 Cut(const mat4& mat, int row, int col)
+{
+    mat3 result;
+    int index = 0;
+    int target;
+    int source;
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            if(i == row || j == col)
+            {
+                continue;
+            }
+
+            target = index++;
+            source = 4 * i + j;
+            result.asArray[target] = mat.asArray[source];
+        }
+    }
+}
+
+
+mat2 Minor(const mat2& mat)
+{
+    return mat2(
+        mat._22, mat._21,
+        mat._12, mat._11);
+}
+
+
+mat3 Minor(const mat3& mat)
+{
+    mat3 result;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            result[i][j] = Determinant(Cut(mat, i, j));
+        }
+    }
+
+    return result;
+}
+
+mat4 Minor(const mat4& mat)
+{
+    mat4 result;
+
+    for(int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            result[i][j] = Determinant(Cut(mat, i , j));
+        }
+    }
+    
+    return result;
+}
+
+
+void Cofactor(float* out, const float* minor, int rows, int cols)
+{
+    int t;
+    int s;
+    float sign;
+    
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            s = t = cols * j + i;
+            out[t] = minor[s] * powf(-1.0f, i + j); // + or -
+        }
+    }
+}
+
+mat2 Cofactor(const mat2& mat)
+{
+    mat2 result;
+    Cofactor(result.asArray, Minor(mat).asArray, 2, 2);
+    return result;
+}
+
+mat3 Cofactor(const mat3& mat)
+{
+    mat3 result;
+    Cofactor(result.asArray, Minor(mat).asArray, 3, 3);
+    return result;
+}
+
+mat4 Cofactor(const mat4& mat)
+{
+    mat4 result;
+    Cofactor(result.asArray, Minor(mat).asArray, 4, 4);
+    return result;
+}
+
+mat2 Adjugate(const mat2& mat)
+{
+    return Transpose(Cofactor(mat));
+}
+
+mat3 Adjugate(const mat3& mat)
+{
+    return Transpose(Cofactor(mat));
+}
+
+mat4 Adjugate(const mat4& mat)
+{
+    return Transpose(Cofactor(mat));
+}
+
+mat2 Inverse(const mat2& mat)
+{
+    float det = mat._11 * mat._22 - mat._12 * mat._21;;
+    
+    if(CMP(det, 0.0f)) {return mat2();}
+    mat2 result;
+    float inv_det = 1.0f / det;
+    
+    result._11 = mat._22 * inv_det;
+    result._12 = -mat._12 * inv_det;
+    result._21 = -mat._21 * inv_det;
+    result._22 = mat._11 * inv_det;
+    
+    return result;
+}
+
+mat3 Inverse(const mat3& mat)
+{
+    float det = mat._11 * (mat._22 * mat._33 - mat._23 * mat._32) -
+                mat._12 * (mat._21 * mat._33 - mat._23 * mat._31) +
+                mat._13 * (mat._21 * mat._32 - mat._22 * mat._31);
+    
+    if(CMP(det, 0.0f)) {return mat3();}
+
+    mat3 result;
+    float inv_det = 1.0f / det;
+
+    result._11 = (mat._22 * mat._33 - mat._23 * mat._32) * inv_det;
+    result._12 = -(mat._12 * mat._33 - mat._13 * mat._32) * inv_det;
+    result._13 = (mat._12 * mat._23 - mat._13 * mat._22) * inv_det;
+    result._21 = -(mat._21 * mat._33 - mat._23 * mat._31) * inv_det;
+    result._22 = (mat._11 * mat._33 - mat._13 * mat._31) * inv_det;
+    result._23 = -(mat._11 * mat._23 - mat._13 * mat._21) * inv_det;
+    result._31 = (mat._21 * mat._32 - mat._22 * mat._31) * inv_det;
+    result._32 = -(mat._11 * mat._32 - mat._12 * mat._31) * inv_det;
+    result._33 = (mat._11 * mat._22 - mat._12 * mat._21) * inv_det;
+
+    return result;
+}
+
+mat4 Inverse(const mat4& mat)
+{
+    float det = 
+        mat._11 * (mat._22 * mat._33 * mat._44 - 
+                    mat._23 * mat._34 * mat._42 + 
+                    mat._24 * mat._32 * mat._43 - 
+                    mat._24 * mat._33 * mat._42 + 
+                    mat._23 * mat._34 * mat._41 - 
+                    mat._22 * mat._33 * mat._44);
+
+    if(CMP(det, 0.0f)) {return mat4();}
+
+    float inv_det = 1.0f / det;
+    
+    mat4 result;
+    
+    // First row
+    result._11 = (mat._22 * mat._33 * mat._44 - 
+                  mat._23 * mat._34 * mat._42 + 
+                  mat._24 * mat._32 * mat._43 - 
+                  mat._24 * mat._33 * mat._42 + 
+                  mat._23 * mat._34 * mat._41 - 
+                  mat._22 * mat._33 * mat._44) * inv_det;
+    result._12 = -(mat._21 * mat._33 * mat._44 - 
+                  mat._23 * mat._34 * mat._41 + 
+                  mat._24 * mat._31 * mat._43 - 
+                  mat._24 * mat._33 * mat._41 + 
+                  mat._21 * mat._34 * mat._42 - 
+                  mat._22 * mat._33 * mat._44) * inv_det;
+    result._13 = (mat._21 * mat._32 * mat._44 - 
+                 mat._22 * mat._34 * mat._41 + 
+                 mat._24 * mat._31 * mat._42 - 
+                 mat._23 * mat._34 * mat._41 + 
+                 mat._22 * mat._33 * mat._44 - 
+                 mat._21 * mat._33 * mat._44) * inv_det;
+    result._14 = -(mat._21 * mat._32 * mat._43 - 
+                  mat._22 * mat._33 * mat._41 + 
+                  mat._23 * mat._31 * mat._42 - 
+                  mat._24 * mat._31 * mat._33 + 
+                  mat._24 * mat._32 * mat._31 - 
+                  mat._23 * mat._32 * mat._34) * inv_det;
+
+    // Second row  
+    result._21 = -(mat._12 * mat._33 * mat._44 - 
+                  mat._13 * mat._34 * mat._41 + 
+                  mat._14 * mat._31 * mat._43 - 
+                  mat._14 * mat._33 * mat._41 + 
+                  mat._12 * mat._34 * mat._31 - 
+                  mat._11 * mat._33 * mat._44) * inv_det;
+    result._22 = (mat._11 * mat._34 * mat._44 - 
+                 mat._13 * mat._32 * mat._44 + 
+                 mat._14 * mat._21 * mat._43 - 
+                 mat._14 * mat._33 * mat._41 + 
+                 mat._11 * mat._33 * mat._42 - 
+                 mat._12 * mat._33 * mat._44) * inv_det;
+    result._23 = -(mat._11 * mat._23 * mat._44 - 
+                  mat._12 * mat._24 * mat._41 + 
+                  mat._14 * mat._21 * mat._43 - 
+                  mat._14 * mat._23 * mat._41 + 
+                  mat._12 * mat._24 * mat._31 - 
+                  mat._11 * mat._23 * mat._44) * inv_det;
+    result._24 = (mat._11 * mat._23 * mat._34 - 
+                 mat._12 * mat._24 * mat._31 + 
+                 mat._13 * mat._21 * mat._33 - 
+                 mat._13 * mat._23 * mat._31 + 
+                 mat._12 * mat._24 * mat._21 - 
+                 mat._11 * mat._23 * mat._34) * inv_det;
+
+    // Third row  
+    result._31 = (mat._12 * mat._23 * mat._44 - 
+                 mat._13 * mat._24 * mat._31 + 
+                 mat._14 * mat._21 * mat._32 - 
+                 mat._14 * mat._23 * mat._31 + 
+                 mat._13 * mat._24 * mat._21 - 
+                 mat._11 * mat._23 * mat._34) * inv_det;
+    result._32 = -(mat._11 * mat._24 * mat._34 - 
+                  mat._12 * mat._23 * mat._31 + 
+                  mat._13 * mat._21 * mat._32 - 
+                  mat._13 * mat._22 * mat._31 + 
+                  mat._12 * mat._23 * mat._21 - 
+                  mat._11 * mat._23 * mat._34) * inv_det;
+    result._33 = (mat._11 * mat._22 * mat._34 - 
+                 mat._12 * mat._23 * mat._31 + 
+                 mat._13 * mat._21 * mat._32 - 
+                 mat._13 * mat._22 * mat._31 + 
+                 mat._12 * mat._23 * mat._21 - 
+                 mat._11 * mat._22 * mat._33) * inv_det;
+    result._34 = -(mat._11 * mat._22 * mat._33 - 
+                  mat._12 * mat._23 * mat._31 + 
+                  mat._13 * mat._21 * mat._32 - 
+                  mat._13 * mat._22 * mat._31 + 
+                  mat._12 * mat._23 * mat._21 - 
+                  mat._11 * mat._22 * mat._33) * inv_det;
+
+    // Fourth row  
+    result._41 = -(mat._13 * mat._22 * mat._34 - 
+                  mat._12 * mat._23 * mat._31 + 
+                  mat._13 * mat._21 * mat._32 - 
+                  mat._13 * mat._22 * mat._31 + 
+                  mat._12 * mat._23 * mat._21 - 
+                  mat._11 * mat._23 * mat._34) * inv_det;
+    result._42 = (mat._11 * mat._24 * mat._34 - 
+                 mat._12 * mat._23 * mat._31 + 
+                 mat._13 * mat._21 * mat._32 - 
+                 mat._13 * mat._22 * mat._31 + 
+                 mat._12 * mat._23 * mat._21 - 
+                 mat._11 * mat._23 * mat._34) * inv_det;
+    result._43 = -(mat._11 * mat._22 * mat._33 - 
+                  mat._12 * mat._23 * mat._31 + 
+                  mat._13 * mat._21 * mat._32 - 
+                  mat._13 * mat._22 * mat._31 + 
+                  mat._12 * mat._23 * mat._21 - 
+                  mat._11 * mat._22 * mat._33) * inv_det;
+    result._44 = (mat._11 * mat._22 * mat._33 - 
+                 mat._12 * mat._23 * mat._31 + 
+                 mat._13 * mat._21 * mat._32 - 
+                 mat._13 * mat._22 * mat._31 + 
+                 mat._12 * mat._23 * mat._21 - 
+                 mat._11 * mat._22 * mat._33) * inv_det;
+
+    return result;
+}
+
+mat4 Translation(float x, float y, float z)
+{
+    return {1.0f, 0.0f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 1.0f, 0.0f,
+                x, y, z, 1.0f};
+}
+
+mat4 Translation(const vec3& pos)
+{
+    return {1.0f, 0.0f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 1.0f, 0.0f,
+                pos.x, pos.y, pos.z, 1.0f};
+}
+
+vec3 GetTranslation(const mat4& mat)
+{
+    return vec3(mat._41, mat._42, mat._43);
+}
+
+mat4 Scale(float x, float y, float z)
+{
+    return {x, 0.0f, 0.0f, 0.0f,
+                0.0f, y, 0.0f, 0.0f,
+                0.0f, 0.0f, z, 0.0f,
+                0.0f, 0.0f, 0.0f, 1.0f};
+}
+
+mat4 Scale(const vec3& vec)
+{
+    return {vec.x, 0.0f, 0.0f, 0.0f,
+                0.0f, vec.y, 0.0f, 0.0f,
+                0.0f, 0.0f, vec.z, 0.0f,
+                0.0f, 0.0f, 0.0f, 1.0f};
+}
+
+vec3 GetScale(const mat4& mat)
+{
+    return vec3(mat._11, mat._22, mat._33);
+}
+
+mat3 Rotation3x3(float pitch, float yaw, float roll)
+{
+    return ZRotation3x3(roll) * XRotation3x3(pitch) * YRotation3x3(yaw);
+}
+
+mat4 Rotation(float pitch, float yaw, float roll)
+{
+    return ZRotation(roll) * XRotation(pitch) * YRotation(yaw);
+}
+
+mat3 ZRotation3x3(float angle)
+{
+    angle = DEG2RAD(angle);
+    return {cosf(angle), sinf(angle), 0.0f,
+                -sinf(angle), cosf(angle), 0.0f,
+                0.0f, 0.0f, 1.0f};
+}
+
+mat4 ZRotation(float angle)
+{
+    angle = DEG2RAD(angle);
+    return {cosf(angle), sinf(angle), 0.0f, 0.0f,
+                -sinf(angle), cosf(angle), 0.0f, 0.0f,
+                0.0f, 0.0f, 1.0f, 0.0f,
+                0.0f, 0.0f, 0.0f, 1.0f};
+}
+
+mat3 XRotation3x3(float angle) {
+    angle = DEG2RAD(angle);
+    return mat3(
+    1.0f, 0.0f, 0.0f,
+    0.0f, cosf(angle), sinf(angle),
+    0.0f, -sinf(angle), cos(angle)
+    );
+}
+
+mat4 XRotation(float angle) {
+    angle = DEG2RAD(angle);
+    return mat4(
+    1.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, cosf(angle), sinf(angle), 0.0f,
+    0.0f, -sinf(angle), cos(angle), 0.0f,
+    0.0f, 0.0f, 0.0f, 1.0f
+    );
+}
+
+mat3 YRotation3x3(float angle) {
+    angle = DEG2RAD(angle);
+    return mat3(
+    cosf(angle), 0.0f, -sinf(angle),
+    0.0f, 1.0f, 0.0f,
+    sinf(angle), 0.0f, cosf(angle)
+    );
+}
+
+mat4 YRotation(float angle) {
+    angle = DEG2RAD(angle);
+    return mat4(
+    cosf(angle), 0.0f, -sinf(angle), 0.0f,
+    0.0f, 1.0f, 0.0f, 0.0f,
+    sinf(angle), 0.0f, cosf(angle), 0.0f,
+    0.0f, 0.0f, 0.0f, 1.0f
+    );
+}
+
+mat3 AxisAngle3x3(const vec3& axis, float angle)
+{
+    angle = DEG2RAD(angle);
+    float c = cosf(angle);
+    float s = sinf(angle);
+    float t = 1.0 - cosf(angle);
+    
+    float x = axis.x;
+    float y = axis.y;
+    float z = axis.z;
+
+    if(!CMP(MagnitudeSq(axis), 1.0f))
+    {
+        float inv_len = 1.0f / Magnitude(axis);
+        x *= inv_len;
+        y *= inv_len;
+        z *= inv_len;
+    }
+
+    return {t * (x * x) + c,t * x * y + s * z,t * x * z - s * y,
+                t * x * y - s * z,t * (y * y) + c,t * y * z + s * x,
+                t * x * z + s * y,t * y * z - s * x,t * (z * z) + c};
+}
+
+mat4 AxisAngle(const vec3& axis, float angle)
+{
+    angle = DEG2RAD(angle);
+    float c = cosf(angle);
+    float s = sinf(angle);
+    float t = 1.0 - cosf(angle);
+    
+    float x = axis.x;
+    float y = axis.y;
+    float z = axis.z;
+
+    if(!CMP(MagnitudeSq(axis), 1.0f))
+    {
+        float inv_len = 1.0f / Magnitude(axis);
+        x *= inv_len;
+        y *= inv_len;
+        z *= inv_len;
+    }
+
+    return {t*(x*x) + c, t*x*y + s*z, t*x*z - s*y, 0.0f,
+                t*x*y - s*z, t*(y*y) + c, t*y*z + s*x, 0.0f,
+                t*x*z + s*y, t*y*z - s*x, t*(z*z) + c, 0.0f,
+                0.0f, 0.0f, 0.0f, 1.0f};
 }
--- MathLibrary/MathLibrary/matrices.h
+++ MathLibrary/MathLibrary/matrices.h
@@ -23,6 +23,19 @@ typedef struct mat2
     {
         return &(asArray[i * 2]);
     }
+
+    inline mat2()
+    {
+        _11 = _22 = 1.0f;
+        _12 = _21 = 0.0f;
+    }
+
+    inline mat2(float f11, float f12,
+                float f21, float f22)
+    {
+        _11 = f11; _12 = f12;
+        _21 = f21; _22 = f22;
+    }
 } mat2;
 
 typedef struct mat3
@@ -42,6 +55,22 @@ typedef struct mat3
     inline float* operator[] (int i)
     {
         return &(asArray[i * 3]);
+    }
+
+    inline mat3()
+    {
+        _11 = _22 = _33 = 1.0f;
+        _12 = _13 = _21 = 0.0f;
+        _23 = _31 = _32 = 0.0f;
+    }
+
+    inline mat3(float f11, float f12, float f13,
+                float f21, float f22, float f23,
+                float f31, float f32, float f33)
+    {
+        _11 = f11; _12 = f12; _13 = f13;
+        _21 = f21; _22 = f22; _23 = f23;
+        _31 = f31; _32 = f32; _33 = f33;
     }
 } mat3;
 
@@ -64,6 +93,25 @@ typedef struct mat4
     {
         return &(asArray[i * 4]);
     }
+
+    inline mat4()
+    {
+        _11 = _22 = _33 = _44 = 1.0f;
+        _12 = _13 = _14 = _21 = 0.0f;
+        _23 = _24 = _31 = _32 = 0.0f;
+        _34 = _41 = _42 = _43 = 0.0f;
+    }
+
+    inline mat4(float f11, float f12, float f13, float f14,
+                float f21, float f22, float f23, float f24,
+                float f31, float f32, float f33, float f34,
+                float f41, float f42, float f43, float f44)
+    {
+        _11 = f11; _12 = f12; _13 = f13; _14 = f14;
+        _21 = f21; _22 = f22; _23 = f23; _24 = f24;
+        _31 = f31; _32 = f32; _33 = f33; _34 = f34;
+        _41 = f41; _42 = f42; _43 = f43; _44 = f44;
+    }
 } mat4;
 
 mat2 operator*(const mat2& matrix, const float scalar);
@@ -74,10 +122,45 @@ mat3 operator*(const mat3& matA, const mat3& matB);
 mat4 operator*(const mat4& matA, const mat4& matB);
 
 void Transpose(const float *srcMat, float *dstMat, int srcRows, int srcCols);
-mat2 Transpose(const mat2& matrix);
-mat3 Transpose(const mat3& matrix);
-mat4 Transpose(const mat4& matrix);
+mat2 Transpose(const mat2& mat);
+mat3 Transpose(const mat3& mat);
+mat4 Transpose(const mat4& mat);
 bool Multiply(float* out, const float* matA, int aRows, int aCols, const float* matB, int bRows, int bCols);
+float Determinant(const mat2& mat);
+float Determinant(const mat3& mat);
+float Determinant(const mat4& mat);
+mat2 Cut(const mat3& mat, int row, int col);
+mat3 Cut(const mat4& mat, int row, int col);
+mat2 Minor(const mat2& mat);
+mat3 Minor(const mat3& mat);
+mat4 Minor(const mat4& mat);
+void Cofactor(float* out, const float* minor, int rows, int cols);
+mat2 Cofactor(const mat2& mat);
+mat3 Cofactor(const mat3& mat);
+mat4 Cofactor(const mat4& mat);
+mat2 Adjugate(const mat2& mat);
+mat3 Adjugate(const mat3& mat);
+mat4 Adjugate(const mat4& mat);
+mat2 Inverse(const mat2& mat);
+mat3 Inverse(const mat3& mat);
+mat4 Inverse(const mat4& mat);
+
+mat4 Translation(float x , float y, float z);
+mat4 Translation(const vec3& pos);
+vec3 GetTranslation(const mat4& mat);
+mat4 Scale(float x, float y, float z);
+mat4 Scale(const vec3& vec);
+vec3 GetScale(const mat4& mat);
+mat3 Rotation3x3(float pitch, float yaw, float roll);
+mat4 Rotation(float pitch, float yaw, float roll);
+mat3 ZRotation3x3(float angle);
+mat4 ZRotation(float angle);
+mat3 XRotation3x3(float angle);
+mat4 XRotation(float angle);
+mat3 YRotation3x3(float angle);
+mat4 YRotation(float angle);
+mat3 AxisAngle3x3(const vec3& axis, float angle);
+mat4 AxisAngle(const vec3& axis, float angle);
 
 #endif
 
